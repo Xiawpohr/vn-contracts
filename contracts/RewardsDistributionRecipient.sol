c6170c2b8fb1d3e610e92f54181b435921450ae5
HSIAO PO WEN <xiawpohr@gmail.com>
2020-12-09 19:54:41 +0800

    add StakingRewards contracts

---
+++ contracts/RewardsDistributionRecipient.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.3;
+
+import "./utils/Ownable.sol";
+
+
+abstract contract RewardsDistributionRecipient is Ownable {
+    address public rewardsDistribution;
+
+    function notifyRewardAmount(uint256 reward) external virtual;
+
+    modifier onlyRewardsDistribution() {
+        require(msg.sender == rewardsDistribution, "Caller is not RewardsDistribution contract");
+        _;
+    }
+
+    function setRewardsDistribution(address _rewardsDistribution) external onlyOwner {
+        rewardsDistribution = _rewardsDistribution;
+    }
+}
